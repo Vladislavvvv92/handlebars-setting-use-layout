3bb21daa416d67cebb30b849d80137486bb8cb25
Vladislavvvv92 <Vladislavvvv92@gmail.com>
2019-12-22 14:25:36 +0200

    setting&&use handlebars

---
+++ nodeExpress/init.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const app = express();
+
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+console.log(app);
+app.get('/', (req, res) =>{
+
+    res.render('index')
+
+})
+
+app.get('/about', (req, res) =>{
+
+    res.render('about')
+})
+
+const PORT = process.env.PORT  || 3000;
+
+app.listen(3000, () => {
+    console.log(`Сервер запущен на порте: ${PORT}`)
+});
